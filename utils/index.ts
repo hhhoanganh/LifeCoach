01fd0a2ffb1d92e24b67f7fcae6a1841c1d8ab3c
hhhoanganh <chuhoanganh27@gmail..com>
2023-12-13 21:52:35 +0700

    tesst

---
+++ utils/index.ts
@@ -0,0 +1,28 @@
+export async function fetchCars() {
+    const headers = {
+        'X-RapidAPI-Key': 'fef574c43amsh57025bfb561982dp160d02jsn4bbcb1c3b958',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+        headers:headers,
+    });
+
+    const result = await response.json();
+
+    return result;
+}
+
+export const calcuateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50;
+    const mileageFactor = 0.1;
+    const ageFactor = 0.05;
+
+
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+    return rentalRatePerDay.toFixed(0);
+}
